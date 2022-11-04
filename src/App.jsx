e94b064087296886a748273f6f56653c06ea1082
masp9 <mart2c04@stud.kea.dk>
2022-11-04 12:00:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MainContent from './components/MainContent'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MainContent() {
+    return (
+        <h1>MainContent here</h1>
+    )
+}
